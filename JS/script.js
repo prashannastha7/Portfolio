442ba92ce9289b6018d744bedfb9ff9b93ffcbd9
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-05-15 14:42:07 +0545

    Theme color

--- JS/script.js
+++ JS/script.js
@@ -53,3 +53,20 @@ const typed = new Typed('.multiple-text', {
     backSpeed: 50,
     loop: true
 });
+
+/* Theme light and dark */
+const dayNight = document.querySelector('.day-night');
+dayNight.addEventListener('click', () => {
+    dayNight.querySelector('i').classList.toggle('bxs-sun');
+    dayNight.querySelector('i').classList.toggle('bxs-moon');
+    document.body.classList.toggle('dark');
+})
+
+window.addEventListener('load', () => {
+    if(document.body.classList.contains('dark')) {
+        dayNight.querySelector('i').classList.add('bxs-sun');
+    }
+    else {
+        dayNight.querySelector('i').classList.add('bxs-moon');
+    }
+});
