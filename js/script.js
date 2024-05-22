76f9a43db4e37de49149e2990c4fb0eea1179cf2
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-05-22 20:01:51 +0545

    add js

---
+++ js/script.js
@@ -0,0 +1,72 @@
+/* toggle icon navbar*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/* Scroll sections active link */
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 200;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+});
+
+/*  sticky navbar */
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+/* Scroll Reveal */
+ScrollReveal({
+    reset: true,
+    duration: 2000,
+    distance: '80px',
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content p, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.about-content', { origin: 'right' });
+
+/*typed JS */
+const typed = new Typed('.multiple-text', {
+    strings: ['Backend Developer', 'Designer', 'Student'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+/* Theme light and dark */
+const dayNight = document.querySelector('.day-night');
+dayNight.addEventListener('click', () => {
+    dayNight.querySelector('i').classList.toggle('bxs-sun');
+    dayNight.querySelector('i').classList.toggle('bxs-moon');
+    document.body.classList.toggle('dark');
+})
+
+window.addEventListener('load', () => {
+    if(document.body.classList.contains('dark')) {
+        dayNight.querySelector('i').classList.add('bxs-sun');
+    }
+    else {
+        dayNight.querySelector('i').classList.add('bxs-moon');
+    }
+});
